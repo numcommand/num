39b1e8e5717c5e21f32a9bed55385620e74f20ed
Joel Parker Henderson <joel@joelparkerhenderson.com>
2015-11-16 19:57:09 -0700

    Upgrade testing for tabs and counts

--- implementations/num-awk/test/minitest.sh
+++ implementations/num-awk/test/minitest.sh
@@ -1,16 +1,31 @@
 #!/bin/sh
 
+## Constants
 TRUE=1
 FALSE=0
 TODO="TODO"
 UNDEF="UNDEF"
 
+minitest_start() {
+    success_count=0
+    failure_count=0
+}
+
+minitest_stop() {
+    echo "SUCCESS count: $success_count"
+    echo "FAILURE count: $failure_count"
+}
+
 assert_eq() {
     expect="$1"; actual="$2"; msg="$3"
+    expect_pretty=${expect//$'\n'/␞}; expect_pretty=${expect_pretty//$'\t'/␟};
+    actual_pretty=${actual//$'\n'/␞}; actual_pretty=${actual_pretty//$'\t'/␟}
     if [ "$expect" == "$actual" ]; then
-        echo "assert_eq $msg expect:${expect//$'\n'/␞} actual:${actual//$'\n'/␞} SUCCESS"
+        success_count=$((success_count+1))
+        echo "assert_eq $msg expect:$expect_pretty actual_pretty: SUCCESS"
     else
-        echo "assert_eq $msg expect:${expect//$'\n'/␞} actual:${actual//$'\n'/␞} FAILURE"
+        failure_count=$((failure_count+1))
+        echo "assert_eq $msg expect:$expect_pretty actual_pretty: FAILURE"
     fi
 }
 
