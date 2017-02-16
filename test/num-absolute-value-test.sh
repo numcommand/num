b7c89655a11835129726d375aa45c08dbeccbbf4
Joel Parker Henderson <joel@joelparkerhenderson.com>
2017-02-15 22:23:51 -0800

    Simplify tests

--- test/num-absolute-value-test.sh
+++ test/num-absolute-value-test.sh
@@ -14,8 +14,6 @@ num=${NUM:-num}
 #
 ##
 
-f="absolute-value"
-
 ##
 #
 # ...with all positive...
@@ -37,14 +35,14 @@ f="absolute-value"
 #     5 6 9
 ##
 
-x=$(echo "1 2 3" | "$num" $f) &&
-    assert_eq "1 2 3" "$x" "$f with row with all positive"
+x=$(echo "1 2 3" | "$num" absolute-value) &&
+    assert_eq "1 2 3" "$x" "absolute-value with row with all positive"
 
 x=$(echo "1\n2\n3" | "$num" absolute-value) &&
-    assert_eq "1 2 3" "$x" "$f with col with all positive"
+    assert_eq "1 2 3" "$x" "absolute-value with col with all positive"
 
 x=$(echo "1 2 3\n5 6 9" | "$num" absolute-value records) &&
-    assert_eq "1 2 3"$'\n'"5 6 9" "$x" "$f with records with all positive"
+    assert_eq "1 2 3"$'\n'"5 6 9" "$x" "absolute-value with records with all positive"
 
 ##
 #
@@ -67,11 +65,11 @@ x=$(echo "1 2 3\n5 6 9" | "$num" absolute-value records) &&
 #     5 6 9
 ##
 
-x=$(echo "-1 -2 -3" | "$num" $f) &&
-    assert_eq "1 2 3" "$x" "$f with row with all negative"
+x=$(echo "-1 -2 -3" | "$num" absolute-value) &&
+    assert_eq "1 2 3" "$x" "absolute-value with row with all negative"
 
 x=$(echo "-1\n-2\n-3" | "$num" $f) &&
-    assert_eq "1 2 3" "$x" "$f with col with all negative"
+    assert_eq "1 2 3" "$x" "absolute-value with col with all negative"
 
 x=$(echo "-1 -2 -3\n-5 -6 -9" | "$num" $f records) &&
-    assert_eq "1 2 3"$'\n'"5 6 9" "$x" "$f with records with all negative"
+    assert_eq "1 2 3"$'\n'"5 6 9" "$x" "absolute-value with records with all negative"
--- test/num-coefficient-of-variance-test.sh
+++ test/num-coefficient-of-variance-test.sh
@@ -29,16 +29,14 @@ num=${NUM:-num}
 #     0.31225
 ##
 
-f="coefficient-of-variance"
-
-x=$(echo "1 2 4" | "$num" $f) &&
-    assert_eq 0.654654 "$x" "$f with row"
+x=$(echo "1 2 4" | "$num" coefficient-of-variance) &&
+    assert_eq 0.654654 "$x" "coefficient-of-variance with row"
 
 x=$(echo "1\n2\n4" | "$num" $f) &&
-    assert_eq 0.654654 "$x" "$f with col"
+    assert_eq 0.654654 "$x" "coefficient-of-variance with col"
 
-x=$(echo "1 2 4\n5 6 9" | "$num" $f records) &&
-    assert_eq "0.654654"$'\n'"0.31225" "$x" "$f with records"
+x=$(echo "1 2 4\n5 6 9" | "$num" coefficient-of-variance records) &&
+    assert_eq "0.654654"$'\n'"0.31225" "$x" "coefficient-of-variance with records"
 
 ##
 #
@@ -61,16 +59,14 @@ x=$(echo "1 2 4\n5 6 9" | "$num" $f records) &&
 #     0.31225
 ##
 
-f="sample-coefficient-of-variance"
+x=$(echo "1 2 4" | "$num" sample-coefficient-of-variance) &&
+    assert_eq 0.654654 "$x" "sample-coefficient-of-variance with row"
 
-x=$(echo "1 2 4" | "$num" $f) &&
-    assert_eq 0.654654 "$x" "$f with row"
+x=$(echo "1\n2\n4" | "$num" sample-coefficient-of-variance) &&
+    assert_eq 0.654654 "$x" "sample-coefficient-of-variance with col"
 
-x=$(echo "1\n2\n4" | "$num" $f) &&
-    assert_eq 0.654654 "$x" "$f with col"
-
-x=$(echo "1 2 4\n5 6 9" | "$num" $f records) &&
-    assert_eq "0.654654"$'\n'"0.31225" "$x" "$f with records"
+x=$(echo "1 2 4\n5 6 9" | "$num" sample-coefficient-of-variance records) &&
+    assert_eq "0.654654"$'\n'"0.31225" "$x" "sample-coefficient-of-variance with records"
 
 ##
 #
@@ -93,13 +89,11 @@ x=$(echo "1 2 4\n5 6 9" | "$num" $f records) &&
 #     0.254951
 ##
 
-f="population-coefficient-of-variance"
+x=$(echo "1 2 4" | "$num" population-coefficient-of-variance) &&
+    assert_eq 0.534522 "$x" "population-coefficient-of-variance with row"
 
-x=$(echo "1 2 4" | "$num" $f) &&
-    assert_eq 0.534522 "$x" "$f with row"
+x=$(echo "1\n2\n4" | "$num" population-coefficient-of-variance) &&
+    assert_eq 0.534522 "$x" "population-coefficient-of-variance with col"
 
-x=$(echo "1\n2\n4" | "$num" $f) &&
-    assert_eq 0.534522 "$x" "$f with col"
-
-x=$(echo "1 2 4\n5 6 9" | "$num" $f records) &&
-    assert_eq "0.534522"$'\n'"0.254951" "$x" "$f with records"
+x=$(echo "1 2 4\n5 6 9" | "$num" population-coefficient-of-variance records) &&
+    assert_eq "0.534522"$'\n'"0.254951" "$x" "population-coefficient-of-varianc with records"
